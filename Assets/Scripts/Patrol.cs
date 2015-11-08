b0caa29ee7b89cee61e34d5072de5f6abc54ecbc
Colin Miller <cjmiller@uw.edu>
2015-11-07 21:41:38 -0800

    Making the doctor patrol on a set course

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Patrol : MonoBehaviour {
+	
+	public Transform[] waypoints;
+
+	private NavMeshAgent agent;
+	private int currentWaypoint;
+
+	void Start() {
+		agent = GetComponent<NavMeshAgent> ();
+		currentWaypoint = 0;
+	}
+
+	void Update() {
+		if (agent.remainingDistance < 0.5f) {
+			currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
+			agent.destination = waypoints[currentWaypoint].position;
+		}
+	}
+}
